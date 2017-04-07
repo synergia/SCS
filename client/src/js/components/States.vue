9678c192eeeab485774f79d8d9fbf0d38273dbe8
stsdc <s.dac@yandex.ru>
2017-04-07 17:46:08 +0200

    states styled

--- client/src/js/components/States.vue
+++ client/src/js/components/States.vue
@@ -2,22 +2,16 @@
 <template>
     <div class="states">
         <div class="propulsion" v-for="pin in this.$root.$data.pins.propulsions">
-            <div class="propulsion_num">{{pin.num}}</div>
             <div class="propulsion_name">{{pin.name}}</div>
-            <div class="propulsion_role">{{pin.role}}</div>
             <div class="propulsion_value">{{pin.value}}</div>
         </div>
         <div class="servo" v-for="pin in this.$root.$data.pins.servos">
-            <div class="propulsion_num">{{pin.num}}</div>
-            <div class="propulsion_num">{{pin.name}}</div>
-            <div class="propulsion_num">{{pin.role}}</div>
-            <div class="propulsion_num">{{pin.value}}</div>
+            <div class="servo_name">{{pin.name}}</div>
+            <div class="servo_value">{{pin.value}}</div>
         </div>
         <div class="logic" v-for="pin in this.$root.$data.pins.logics">
-            <div class="propulsion_num">{{pin.num}}</div>
-            <div class="propulsion_num">{{pin.name}}</div>
-            <div class="propulsion_num">{{pin.role}}</div>
-            <div class="propulsion_num">{{pin.value}}</div>
+            <div class="logic_name">{{pin.name}}</div>
+            <div class="logic_value">{{pin.value}}</div>
         </div>
     </div>
 </template>
