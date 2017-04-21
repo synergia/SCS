928c50355c014d1081460bd3a2648609c9e74ab2
stsdc <s.dac@yandex.ru>
2017-04-21 17:18:47 +0200

    #12 empty state for stream

--- client/src/js/ui/components/Stream.vue
+++ client/src/js/ui/components/Stream.vue
@@ -1,5 +1,28 @@
 <template>
 <div class="stream">
-    <img class="stream_img" src="http://10.42.0.251:4000/?action=stream"></img>
+    <img id="stream" class="stream_img" src="http://10.42.0.251:4000/?action=stream"></img>
+    <div class="stream_noSignal">No signal</div>
 </div>
 </template>
+<script>
+module.exports = {
+
+    methods: {
+
+    },
+    components: {
+
+    },
+    mounted: function() {
+        let stream = document.getElementById('stream');
+        stream.onerror = function (e) {
+            stream.classList.add('stream-hide');
+        }
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
