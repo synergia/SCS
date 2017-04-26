e2f4f3f7605f16b77c44b61d4408ad8395331f44
stsdc <s.dac@yandex.ru>
2017-04-27 01:22:02 +0200

    webpack2 build

--- webpack.config.js
+++ webpack.config.js
@@ -1,111 +1,167 @@
-var path = require('path');
-var webpack = require('webpack');
+var
+    path = require('path'),
+    webpack = require('webpack'),
+    // cssUtils = require('./css-utils'),
+    // env = require('./env-utils'),
+    // HtmlWebpackPlugin = require('html-webpack-plugin'),
+    projectRoot = __dirname + '../';
+// projectRoot = path.resolve(__dirname, '../');
+// entry = './js/main.js';
+var merge = require('webpack-merge');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
+    devtool: '#eval-source-map',
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true
+    },
     context: __dirname + '/client/src',
     entry: {
         app: './js/main.js',
-        // styles: './styles/main.scss',
         vendor: [
-            "socket.io-client",
-            "vue",
-            "vue-router",
-            "keyboardjs",
-            "nipplejs",
-            "hammerjs",
-            "vue-slider-component"
-        ]
+        "socket.io-client",
+        "vue",
+        "vue-router",
+        "keyboardjs",
+        "nipplejs",
+        "hammerjs",
+        "vue-slider-component"
+    ]
     },
     output: {
         path: __dirname + '/client/dist',
-        filename: "[name].js",
-        library: "[name]"
+        publicPath: '/',
+        filename: '[name].js',
+        library: '[name]'
     },
     resolve: {
-        modulesDirectories: ['node_modules'],
-        extensions: ['', '.js', '.sass', '.css'],
-    },
-    resolveLoader: {
-        modulesDirectories: ['node_modules'],
-        moduleTemplates: ['*-loader', '*'],
-        extensions: ['', '.js']
+        extensions: ['.js', '.vue', '.scss', '.css'],
+        modules: [
+      path.join(__dirname, '../'),
+      'node_modules'
+    ],
+        // alias: {
+        //   assets: path.resolve(__dirname, '../dev/assets'),
+        //   components: path.resolve(__dirname, '../dev/components'),
+        //   data: path.resolve(__dirname, '../dev/data')
+        // }
     },
     module: {
-        loaders: [
+        rules: [
+            { // eslint
+                enforce: 'pre',
+                test: /\.(vue|js)$/,
+                loader: 'eslint-loader',
+                include: projectRoot,
+                exclude: /node_modules/
+      },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                include: projectRoot,
+                exclude: /node_modules/
+      },
             {
                 test: /\.vue$/,
-                loader: 'vue'
-            },
+                loader: 'vue-loader',
+                options: {
+                    //   postcss: cssUtils.postcss,
+                    loaders: merge({
+                            js: 'babel-loader'
+                        }
+                        // cssUtils.styleLoaders({sourceMap: true})
+                    )
+                }
+      },
             {
-                test: /\.scss$/,
-                loaders: ['style', 'css?sourceMap', 'sass?sourceMap']
+                test: /\.(scss|css)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: [{
+                        loader: 'style-loader',
+            }],
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localIdentName: '[name]__[local]--[hash:base64:5]',
+                        },
+            }, {
+                        loader: 'sass-loader',
+            }],
+                })
                 // loader: ExtractTextPlugin.extract(
                 //     'style', // The backup style loader
                 //     'css?sourceMap!sass?sourceMap'
                 // )
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel',
-                exclude: /node_modules/,
-                query: {
-                    "presets": ["es2015"],
-                    "plugins": ["add-module-exports"]
-                },
       },
+    //         {
+    //             test: /\.svg$/,
+    //             loader: 'raw'
+    //   },
             {
                 test: /\.json$/,
-                loader: 'json'
+                loader: 'json-loader'
       },
-    //         {
-    //             test: /\.html$/,
-    //             loader: 'vue-html'
-    //   },
-    //         {
-    //             test: /\.(png|jpg|gif|svg)$/,
-    //             loader: 'url',
-    //             query: {
-    //                 limit: 10000,
-    //                 name: '[name].[ext]?[hash]'
-    //             }
-    //   },
             {
-                test: /\.(png|jpg|svg|ttf|eot|woff|woff2)$/,
-                loader: 'url'
-}
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'img/[name].[hash:7].[ext]'
+                }
+      },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    name: 'fonts/[name].[hash:7].[ext]'
+                }
+      }
     ]
     },
-    // Allows to use sass in *.vue
-
-    devtool: '#source-map',
     plugins: [
-    new ExtractTextPlugin("assets/styles/app.css"),
-    function() {
-            this.plugin('watch-run', function(watching, callback) {
-                console.log('\n' + new Date() + '\n');
-                callback();
-            });
-        },
-    new webpack.optimize.CommonsChunkPlugin( /* chunkName= */ "vendor", /* filename= */ "vendor.js")
-
- ]
+    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.DefinePlugin({
+    //   'process.env': {
+    //     NODE_ENV: '"development"'
+    //   },
+    //   '__THEME': '"' + env.platform.theme + '"'
+    // }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // new HtmlWebpackPlugin({
+    //   filename: 'index.html',
+    //   template: 'dev/index.html',
+    //   inject: true
+    // }),
+    new webpack.LoaderOptionsPlugin({
+            options: {
+                context: path.resolve(__dirname, '../src'),
+                //     eslint: {
+                //       formatter: require('eslint-friendly-formatter')
+                //     },
+                //     // postcss: cssUtils.postcss
+            }
+        }),
+    new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function(module) {
+                // this assumes your vendor imports exist in the node_modules directory
+                return module.context && module.context.indexOf('node_modules') !== -1;
+            }
+        }),
+        //CommonChunksPlugin will now extract all the common modules from vendor and main bundles
+    new webpack.optimize.CommonsChunkPlugin({
+            name: 'manifest' //But since there are no more common modules between them we end up with just the runtime code included in the manifest file
+        }),
+    new ExtractTextPlugin({
+            filename: 'index.css',
+            disable: false,
+            allChunks: true
+        }),
+  ],
+    performance: {
+        hints: false
+    }
 };
-
-if (process.env.NODE_ENV === 'production') {
-    module.exports.devtool = '#source-map';
-    // http://vue-loader.vuejs.org/en/workflow/production.html
-    module.exports.plugins = (module.exports.plugins || []).concat([
-    new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: '"production"'
-            }
-        }),
-    new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        }),
-    new webpack.optimize.OccurenceOrderPlugin()
-]);
-}
